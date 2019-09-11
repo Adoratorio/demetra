8bfd335b9e73d0b24013f434fbffeb3f8bca2a7b
Daniele Borra <daniele@adoratorio.com>
2019-09-11 17:03:28 +0200

    Fix for url validation regexp

--- src/validate.ts
+++ src/validate.ts
@@ -1,4 +1,4 @@
 export const validateUrl = (url : string) => {
-  const pattern = new RegExp(/^(http|https):\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/);
+  const pattern = new RegExp(/^(http|https):\/\/[\w\-\.]*(:[0-9]+)?\/?(api)*(\.php)*$/i);
   return pattern.test(url);
 }
