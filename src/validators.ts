672f2a66ebdbd6470af43a91c489bff9b09e83df
Daniele Borra <daniele@adoratorio.com>
2021-05-17 12:01:23 +0200

    Fix for url validation

--- src/validators.ts
+++ src/validators.ts
@@ -2,5 +2,5 @@
 
 export const validateUrl = (url: string): boolean => {
   const pattern = new RegExp(/^(http|https):\/\/[\w\-.]*(:[0-9]+)?\/?(api)*(\.php)*$/i);
-  return pattern.test(url);
+  return pattern.test(url) || url.startsWith('/');
 };
