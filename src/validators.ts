66b6939548d826ceb3a98c90b02c29db4dfa08d1
biazoadoratorio <79836113+biazoadoratorio@users.noreply.github.com>
2023-02-08 09:56:34 +0000

    Fix for endpoint url regex

--- src/validators.ts
+++ src/validators.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const validateUrl = (url: string): boolean => {
-  const pattern = new RegExp(/^(http|https):\/\/[\w\-.]*(:[0-9]+)?\/?(api)*(\.php)*$/i);
+  const pattern = new RegExp(/^(http|https):\/\/[\w\-.]*(:[0-9]+)?\/\w*\/?(api)*(\.php)*$/i);
   return pattern.test(url) || url.startsWith('/');
 };
