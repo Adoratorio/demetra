49f89bc3bc6ac00227df14835f7fd1040fbf5304
Daniele <daniele@adoratorio.com>
2025-05-29 16:10:01 +0200

    Removed md5 packet

--- src/Requests/DemetraRequest.ts
+++ src/Requests/DemetraRequest.ts
@@ -1,4 +1,4 @@
-import * as md5 from "md5";
+import { createHash } from "crypto";
 import { WP_MODES } from "../declarations";
 
 class DemetraRequest {
@@ -28,7 +28,7 @@ class DemetraRequest {
 
   public get hash() {
     if (this.md5.length > 0 ) return this.md5;
-    this.md5 = md5(JSON.stringify(this));
+    this.md5 = createHash('md5').update(JSON.stringify(this)).digest('hex');
     return this.md5;
   }
 }
