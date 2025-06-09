88ea74f69cd8beaeedef8719fd8dc6c017bddd5d
Daniele <daniele@adoratorio.com>
2025-06-09 12:55:13 +0200

    Back to md5 package

--- src/Requests/DemetraRequest.ts
+++ src/Requests/DemetraRequest.ts
@@ -1,4 +1,4 @@
-import { createHash } from "crypto";
+import * as md5 from "md5";
 import { WP_MODES } from "../declarations";
 
 class DemetraRequest {
@@ -28,7 +28,7 @@ class DemetraRequest {
 
   public get hash() {
     if (this.md5.length > 0 ) return this.md5;
-    this.md5 = createHash('md5').update(JSON.stringify(this)).digest('hex');
+    this.md5 = md5(JSON.stringify(this));
     return this.md5;
   }
 }
